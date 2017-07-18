4e2541fb1da99062874eb6c5c0552cb0afa8872a
skyamz <920399454@qq.com>
2017-07-18 16:02:00 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,315 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import tensorflow as tf
+
+#这些参数是根据经验以及结果来调整的，此参数组合可以让准确率到0.99
+LEARNING_RATE = 1e-4 #神经网络的学习率，先初始化为1e-4
+TRAINING_ITERATIONS = 2500#迭代的次数
+DROPOUT = 0.5#后期使用dropout算法来防止过拟合，dropout值选择0.5,说明一层神经元会有一半将被屏蔽，使其激活址值为0
+BATCH_SIZE = 50#每50个样本用于计算梯度下降算法
+VALIDATION_SIZE = 2000
+IMAGE_TO_DISPLAY = 10 #显示第十张图片
+
+#读入训练集，在这之前要将csv保存为utf-8格式，否则会出现中文乱码的问题
+data = pd.read_csv('//Users//skyamz//Desktop//kaggledigit数据//train.csv')
+#查看数据集有多少行多少列，显示数据集的前五行
+print('data({0[0]},{0[1]})'.format(data.shape))
+print (data.head())
+
+#一行代表一张图，从第一列开始获取数据，因为第0列是"label"，即准确的是数字几的结果。不作为训练的数据，只用作结果的比对。
+images = data.iloc[:,1:].values
+#由于我们要将图像灰化，即把每个pixel都归一，所以要对pixel进行计算。将原本的int转化为float，方便观察归一化的结果。
+images = images.astype(np.float)
+# convert from [0:255] => [0.0:1.0]
+#经过统计，最大的pixel为255，所以给images矩阵都乘以1.0 / 255.0来进行归一化处理
+#像素值越接近于1，就越黑，越接近于0越白
+images = np.multiply(images, 1.0 / 255.0)
+#查看一下现在有多少张图片
+print('images({0[0]},{0[1]})'.format(images.shape))
+
+
+#images的shape属性现在为[42000，784]，所以images.shape[1]为784。即有42000张图片，每张有784个piexel来描述
+image_size = images.shape[1]
+print ('image_size => {0}'.format(image_size))
+# 一张图片，是用28*28=784个像素来表示的，即一个边长为28的正方形，所以image_width = image_height = 28
+image_width = image_height = np.ceil(np.sqrt(image_size)).astype(np.uint8)
+print ('image_width => {0}\nimage_height => {1}'.format(image_width,image_height))
+
+
+# 将其中一个图像画出来，观察一下样子
+def display(img):
+    # (784) => (28,28)
+    #将img矩阵从一行784列重塑成28行28列
+    one_image = img.reshape(image_width, image_height)
+    plt.axis('off')#不显示坐标尺寸
+    plt.imshow(one_image, cmap=cm.gray)
+    #这里的binary，我查不到具体含义，个人理解是以二进制的形式呈现图像。背景为白色，像素值越接近于1，就越黑，越接近于0越白
+    #我将binary改变成gray后，全部的黑白色都反转了
+# output image
+display(images[IMAGE_TO_DISPLAY])#显示第十个图片，查源数据可知应该显示数字'8'
+
+
+#ravel()函数返回的是这一列的视图，改变labels_flat的值时，原始的'label'也会改变
+#如果不想改变原始的'label'，则这里可以使用flatten()
+#labels_flat在这里存放的是结果集，即判断前面images是哪个数字
+labels_flat = data['label'].values.ravel()
+print('labels_flat({0})'.format(len(labels_flat)))#一共有42000个图片
+print ('labels_flat[{0}] => {1}'.format(IMAGE_TO_DISPLAY,labels_flat[IMAGE_TO_DISPLAY]))#试着去查找第十个数，看输出是否为8
+#统计有多少类数字，输出结果为10，即总共有十个数字，0-9
+labels_count = np.unique(labels_flat).shape[0]
+print('labels_count => {0}'.format(labels_count))
+
+
+# convert class labels from scalars to one-hot vectors
+# 0 => [1 0 0 0 0 0 0 0 0 0]
+# 1 => [0 1 0 0 0 0 0 0 0 0]
+# ...
+# 9 => [0 0 0 0 0 0 0 0 0 1]
+#将结果集进行one-hot编码，具体见上面的注释说明
+def dense_to_one_hot(labels_dense, num_classes):
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+labels = dense_to_one_hot(labels_flat, labels_count)
+labels = labels.astype(np.uint8)
+print('labels({0[0]},{0[1]})'.format(labels.shape))
+print ('labels[{0}] => {1}'.format(IMAGE_TO_DISPLAY,labels[IMAGE_TO_DISPLAY]))
+
+# split data into training & validation
+validation_images = images[:VALIDATION_SIZE]
+validation_labels = labels[:VALIDATION_SIZE]
+train_images = images[VALIDATION_SIZE:]
+train_labels = labels[VALIDATION_SIZE:]
+print('train_images({0[0]},{0[1]})'.format(train_images.shape))
+print('validation_images({0[0]},{0[1]})'.format(validation_images.shape))
+
+#TensorFlow graph
+# weight initialization
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+# convolution
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+# pooling
+# [[0,3],
+#  [4,2]] => 4
+
+# [[0,1],
+#  [1,1]] => 1
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+# input & output of NN
+# images
+x = tf.placeholder('float', shape=[None, image_size])
+# labels
+y_ = tf.placeholder('float', shape=[None, labels_count])
+
+# first convolutional layer
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+# (40000,784) => (40000,28,28,1)
+image = tf.reshape(x, [-1,image_width , image_height,1])
+#print (image.get_shape()) # =>(40000,28,28,1)
+h_conv1 = tf.nn.relu(conv2d(image, W_conv1) + b_conv1)
+#print (h_conv1.get_shape()) # => (40000, 28, 28, 32)
+h_pool1 = max_pool_2x2(h_conv1)
+#print (h_pool1.get_shape()) # => (40000, 14, 14, 32)
+# Prepare for visualization
+# display 32 fetures in 4 by 8 grid
+layer1 = tf.reshape(h_conv1, (-1, image_height, image_width, 4 ,8))
+# reorder so the channels are in the first dimension, x and y follow.
+layer1 = tf.transpose(layer1, (0, 3, 1, 4,2))
+layer1 = tf.reshape(layer1, (-1, image_height*4, image_width*8))
+
+
+# second convolutional layer
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+#print (h_conv2.get_shape()) # => (40000, 14,14, 64)
+h_pool2 = max_pool_2x2(h_conv2)
+#print (h_pool2.get_shape()) # => (40000, 7, 7, 64)
+# Prepare for visualization
+# display 64 fetures in 4 by 16 grid
+layer2 = tf.reshape(h_conv2, (-1, 14, 14, 4 ,16))
+# reorder so the channels are in the first dimension, x and y follow.
+layer2 = tf.transpose(layer2, (0, 3, 1, 4,2))
+layer2 = tf.reshape(layer2, (-1, 14*4, 14*16))
+
+# densely connected layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+# (40000, 7, 7, 64) => (40000, 3136)
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+print (h_fc1.get_shape())
+# => (40000, 1024)
+
+
+# dropout
+keep_prob = tf.placeholder('float')
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+# readout layer for deep net
+W_fc2 = weight_variable([1024, labels_count])
+b_fc2 = bias_variable([labels_count])
+y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+print (y.get_shape())
+# => (40000, 10)
+
+
+# cost function
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+# optimisation function
+train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+# evaluation
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+# prediction function
+#[0.1, 0.9, 0.2, 0.1, 0.1 0.3, 0.5, 0.1, 0.2, 0.3] => 1
+predict = tf.argmax(y,1)
+
+
+#Train, validate and predict
+epochs_completed = 0
+index_in_epoch = 0
+num_examples = train_images.shape[0]
+
+# serve data by batches
+def next_batch(batch_size):
+    global train_images
+    global train_labels
+    global index_in_epoch
+    global epochs_completed
+
+    start = index_in_epoch
+    index_in_epoch += batch_size
+
+    # when all trainig data have been already used, it is reorder randomly
+    if index_in_epoch > num_examples:
+        # finished epoch
+        epochs_completed += 1
+        # shuffle the data
+        perm = np.arange(num_examples)
+        np.random.shuffle(perm)
+        train_images = train_images[perm]
+        train_labels = train_labels[perm]
+        # start next epoch
+        start = 0
+        index_in_epoch = batch_size
+        assert batch_size <= num_examples
+    end = index_in_epoch
+    return train_images[start:end], train_labels[start:end]
+
+# start TensorFlow session
+init = tf.initialize_all_variables()
+sess = tf.InteractiveSession()
+sess.run(init)
+
+# visualisation variables
+train_accuracies = []
+validation_accuracies = []
+x_range = []
+
+display_step = 1
+
+for i in range(TRAINING_ITERATIONS):
+
+    # get new batch
+    batch_xs, batch_ys = next_batch(BATCH_SIZE)
+
+    # check progress on every 1st,2nd,...,10th,20th,...,100th... step
+    if i % display_step == 0 or (i + 1) == TRAINING_ITERATIONS:
+
+        train_accuracy = accuracy.eval(feed_dict={x: batch_xs,
+                                                  y_: batch_ys,
+                                                  keep_prob: 1.0})
+        if (VALIDATION_SIZE):
+            validation_accuracy = accuracy.eval(feed_dict={x: validation_images[0:BATCH_SIZE],
+                                                           y_: validation_labels[0:BATCH_SIZE],
+                                                           keep_prob: 1.0})
+            print('training_accuracy / validation_accuracy => %.2f / %.2f for step %d' % (
+            train_accuracy, validation_accuracy, i))
+
+            validation_accuracies.append(validation_accuracy)
+            validation_accuracy = accuracy.eval(feed_dict={x: validation_images,
+                                                           y_: validation_labels,
+                                                           keep_prob: 1.0})
+            print('validation_accuracy => %.4f' % validation_accuracy)
+            plt.plot(x_range, train_accuracies, '-b', label='Training')
+            plt.plot(x_range, validation_accuracies, '-g', label='Validation')
+            plt.legend(loc='lower right', frameon=False)
+            plt.ylim(ymax=1.1, ymin=0.7)
+            plt.ylabel('accuracy')
+            plt.xlabel('step')
+            plt.show()
+
+        else:
+            print('training_accuracy => %.4f for step %d' % (train_accuracy, i))
+        train_accuracies.append(train_accuracy)
+        x_range.append(i)
+
+        # increase display_step
+        if i % (display_step * 10) == 0 and i:
+            display_step *= 10
+    # train on batch
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: DROPOUT})
+
+
+
+# read test data from CSV file
+test_images = pd.read_csv('../input/test.csv').values
+test_images = test_images.astype(np.float)
+
+# convert from [0:255] => [0.0:1.0]
+test_images = np.multiply(test_images, 1.0 / 255.0)
+
+print('test_images({0[0]},{0[1]})'.format(test_images.shape))
+
+
+# predict test set
+#predicted_lables = predict.eval(feed_dict={x: test_images, keep_prob: 1.0})
+
+# using batches is more resource efficient
+predicted_lables = np.zeros(test_images.shape[0])
+for i in range(0,test_images.shape[0]//BATCH_SIZE):
+    predicted_lables[i*BATCH_SIZE : (i+1)*BATCH_SIZE] = predict.eval(feed_dict={x: test_images[i*BATCH_SIZE : (i+1)*BATCH_SIZE],
+                                                                                keep_prob: 1.0})
+
+
+print('predicted_lables({0})'.format(len(predicted_lables)))
+
+# output test image and prediction
+display(test_images[IMAGE_TO_DISPLAY])
+print ('predicted_lables[{0}] => {1}'.format(IMAGE_TO_DISPLAY,predicted_lables[IMAGE_TO_DISPLAY]))
+
+# save results
+np.savetxt('submission_softmax.csv',
+           np.c_[range(1,len(test_images)+1),predicted_lables],
+           delimiter=',',
+           header = 'ImageId,Label',
+           comments = '',
+           fmt='%d')
+
+layer1_grid = layer1.eval(feed_dict={x: test_images[IMAGE_TO_DISPLAY:IMAGE_TO_DISPLAY+1], keep_prob: 1.0})
+plt.axis('off')
+plt.imshow(layer1_grid[0], cmap=cm.seismic )
+
+sess.close()
+
+
